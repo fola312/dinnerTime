d0f5a4a4d4fb3b2e8ce0ae8c6854172821e99ca5
Fola Orokunle <robingoodfellow17@users.noreply.github.com>
2019-05-28 18:25:15 -0400

    material-ized colors

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Footer = () => {
      return (
-        <footer className="pv4 ph3 ph5-m ph6-l white">
+        <footer className="pv4 ph3 ph5-m ph6-l black">
           <small className="f4 db tc">© 2019 <b className="ttu">Too Many Open Tabs Inc</b>., All Rights Reserved</small>
           <small className="f5 db tc">Powered by React and 4 to 5 mozzarella sticks</small>
         </footer>
--- src/header.js
+++ src/header.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = () => {
      return (
         <div className=''>
-            <a className="f3 no-underline br-pill ba bw2 ph3 pv2 mb2 dib dark-green bg-white i tc" href="/"><h1>Whats for Dinner Fola?</h1></a>
+            <a className="f3 no-underline br-pill ba bw2 ph3 pv2 mb2 dib black bg-white i tc" href="/"><h1>Whats for Dinner Fola?</h1></a>
         </div>
     );
 }
